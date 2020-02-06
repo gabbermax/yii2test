4584d8fe4eb58a2b21fb1804f0c59e4bf542542e
gabbermax <c0unt.0@yandex.ru>
2020-02-07 01:07:50 +0300

    Update ConsoleController.php

--- ConsoleController.php
+++ ConsoleController.php
@@ -9,6 +9,10 @@ namespace app\commands;
 use yii\console\Controller;
 use yii\console\ExitCode;
 use yii\helpers\Console;
+//use yii\base\Model;
+//use yii\model\Consoledb;
+//use yii\i18n\Formatter;
+use app\models\Images;
 
 /**
  * This command echoes the first argument that you have entered.
@@ -18,7 +22,7 @@ use yii\helpers\Console;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class HelloController extends Controller
+class ConsoleController extends Controller
 {
     /**
      * This command echoes what you have entered as the message.
@@ -31,12 +35,24 @@ class HelloController extends Controller
         return ExitCode::OK;
     }
 
-     public function actionDrum($message = 'Drum') {
+     public function actionDrumina($message = 'test') {
      // данный контроллер будет запускаться  раз в день
      //получаем из бд записи , сортируем по дате
          //проверяем по очереди не меньше ли дата текущей 
                     //если да - ищем id папки и удаляем ее 
          
+           //подключаемся к бд    
+           $query = new Images;
+        //var_dump($this);
+       // ($query->getDb());
+        $result=$query->find()
+            ->where(['id'=>1])
+             ->all();
+        
+        print_r($result[0]);
+        echo '<br>';
+       
+         //недоделано
          
      }
 
